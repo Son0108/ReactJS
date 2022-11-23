4d94c21afe2ca33c35e1bd16a9d94e59d5906535
Son0108 <nguyentatson12346789@gmail.com>
2022-11-23 14:34:04 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Products from './Products/Products';
+import {Route, Link, NavLink, Routes, Outlet } from 'react-router-dom';
+import About from './About';
 
 
 function App() {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     const fetchData = async () => {
       let json = await fetch('http://localhost:5050/products');
@@ -18,7 +19,24 @@ function App() {
   
   return (
     <div className="App">
-      <Products products={products} setProducts={setProducts}/>
+      <div className='header'>
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>HOME</a>
+            </li>
+            <li>
+              <a href="/about">ABOUT</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className='content'>
+      <Routes>
+        <Route path="/"element={<Products products={products}/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+      </div>
     </div>
   );
 }
--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Buttons/Button'
 import './Popup.css'
 
 function Popup({Props, trigger, setTrigger}) {
 
+    const handleChange = (e) => {
+        Object.getOwnPropertyNames(Props).map((id) => {
+            console.log(Props[id])
+        })
+    }
+
+    const handleSave = () => {
+        alert('Đã lưu');
+    }
     return (trigger) ? (
     <div className='popup'>
         <div className='popup_inner'>
         { Object.keys(Props).map((key, i) => (
             <div key={i}>
-            <label>{key}</label><br/>
-            <input type="text" defaultValue={Props[key]}></input>
+                <label>{key}</label><br/>
+                <input 
+                    onChange={handleChange}
+                    type="text" defaultValue={Props[key]}
+                ></input>
             </div>
         ))}
             <div className='button_popup'>
-                <Button title="Save" handle={handleSave} color="green"/>
+                <Button title="Save" handle={() => handleSave()} color="green"/>
                 <Button title="Cancel" handle={() => setTrigger(false)} color="red"/>   
             </div>
         </div>
@@ -22,8 +34,5 @@ function Popup({Props, trigger, setTrigger}) {
     ) : "";
 }
 
-function handleSave() {
-    alert('Đã lưu');
-}
 
 export default Popup
